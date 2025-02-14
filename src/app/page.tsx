3f4a5db73d0c9f5b9091f9593b25e90b646b7c0d
elltja <estq2009@gmail.com>
2025-02-14 19:35:14 +0100

    Responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function TodoPage() {
   return (
-    <div className="w-screen h-screen flex flex-1 gap-3 justify-center">
+    <div className="w-screen h-screen flex flex-col flex-1 gap-3 justify-center flex-wrap md:flex-row ">
       <TodoBox />
       <ProfileCard />
     </div>
